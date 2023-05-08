df6bd0bc6d4f88d7ec337eef2ade66443230ebc8
tototobo <63040524+tototobo@users.noreply.github.com>
2023-05-08 16:23:46 -0400

    first streamlit

---
+++ streamlit.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load data
+data = pd.read_csv('2019_empirical_electricity_prices.csv')
+
+# Create plot
+fig, ax = plt.subplots()
+ax.plot(data['Time_Index'], data['Prices'])
+ax.set_xlabel('Date')
+ax.set_ylabel('Price')
+ax.set_title('Electricity Prices Over Time')
+
+# Display plot
+st.pyplot(fig)
